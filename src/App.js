98d9e9607c31c53697e9e1310763eddddc1ee686
kwamekyeimonies <kwamekyeimonies@gmail.com>
2022-12-27 22:12:34 +0000

    props drilling challenge

--- src/App.js
+++ src/App.js
@@ -1,79 +1,17 @@
 import React, { useState } from 'react'
-import Additem from './Components/Additem'
-import Content from './Components/Content'
-import Footer from './Components/Footer'
-import Header from './Components/Header'
-import SearchItem from './Components/SearchItem'
+import Input from './Components/Input'
+import Square from './Components/Square'
 
-const App = () => { 
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem("ShoppingList")) || [
-    {
-        id:1,
-        checked:true,
-        item:"First Item"
-    },
-    {
-        id:2,
-        checked:false,
-        item:"Second Item"
-    },
-    {
-        id:3,
-        checked:false,
-        item:"Item 3"
-    }
-])
+const App = () => {
 
-const [newItem, setNewItem] = useState('');
-const [search,setSearch] = useState(" ");
-
-const setAndSaveItems = (newItems)=>{
-  setItems(newItems)
-  localStorage.setItem("ShoppingList",JSON.stringify(newItems))
-}
-
-const addItem =(item)=>{
-  const id = items.length ? items[items.length-1].id+1 : 1
-  const myNewItem = {id, checked:false , item}
-  const listItems= [...items,myNewItem]
-  setAndSaveItems(listItems)
-}
-
-const handleCheck = (id) =>{
-  // console.log(`key: ${id}`)
-  const listItems = items.map((item)=> item.id === id ? {...item, checked: !item.checked} : item)
-  setAndSaveItems(listItems)
-
-}
-
-const handleDelete = (id) =>{
-  // console.log(id)
-  const listItems = items.filter((item)=> item.id !== id);
-  console.log(listItems)
- setAndSaveItems(listItems)
-}
-
-const handleSubmit=(e)=>{
-  e.preventDefault()
-  if (!newItem) return;
-  addItem(newItem)
-  setNewItem('')
-  console.log(newItem)
-
-}
-
+  const [colorValue, setColorValue] = useState(" ")
+  const [hexValue,setHexValue] = useState('')
+  const [isDarkText,setIsDarkText] = useState(true)
 
   return (
-    <div className='App'> 
-      <Header title="Training Session" />
-      <Additem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
-      <SearchItem search={search} setSearch={setSearch} />
-      <Content 
-      items={items.filter(item=>((item.item).toLowerCase()).includes(search.toLowerCase()))}
-      handleCheck={handleCheck}
-      handleDelete={handleDelete}
-      />
-      <Footer length={items.length} />
+    <div className='App'>
+      <Square colorValue={colorValue} hexValue={hexValue} isDarkText={isDarkText}    />
+      <Input colorValue={colorValue} setColorValue={setColorValue}  setHexValue={setHexValue} setIsDarkText={setIsDarkText} />
     </div>
   )
 }
---
+++ src/Components/Input.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import colorNames from 'colornames';
+
+const Input = ({colorValue,setColorValue,setHexValue, setIsDarkText, isDarkText}) => {
+  return (
+    <form onSubmit={(e)=>e.preventDefault()}>
+        <label>Add Color Name: </label>
+        <input
+        autoFocus
+        type="text"
+        placeholder='Add Color name'
+        required
+        value={colorValue}
+        onChange={(e)=>{
+            setColorValue(e.target.value)
+            setHexValue(colorNames(e.target.value))
+        }}
+        />
+        <button type='button' onClick={()=>setIsDarkText(!isDarkText)}>Toggle Text Color</button>
+    </form>
+  )
+}
+
+export default Input
